c7012f88f2c0dbe7e835440673cde9295f12bb3a
Noppakaow Matwanna <noppakaowmt@gmail.com>
2026-07-17 22:07:53 +0700

    new

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,21 +1,35 @@
 const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req, res, next) => {
-  try {
-    const token = req.headers.authorization?.split(" ")[1];
+module.exports = (req, res, next) => {
 
-    if (!token) {
-      return res.status(401).json({ message: "No token provided" });
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({
+            message: "No token provided"
+        });
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = decoded; // { id, role }
+    const parts = authHeader.split(" ");
+    if (parts.length !== 2 || parts[0] !== "Bearer") {
+        return res.status(401).json({
+            message: "Invalid token format"
+        });
+    }
 
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
+    const token = parts[1];
+    jwt.verify(
+        token,
+        process.env.JWT_SECRET,
+        (err, user) => {
+            if (err) {
+                return res.status(403).json({
+                    message: "Invalid token"
+                });
+            }
+            req.user = user;
+            next();
+        }
+    );
 };
-
-module.exports = authMiddleware;
--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,45 @@ const router = express.Router();
 
 const authController = require("../controllers/authController");
 
-// public routes
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+const auth = require("../middlewares/authMiddleware");
+const role = require("../middlewares/roleMiddleware");
+
+
+// =====================
+// LOGIN
+// =====================
+router.post(
+    "/login",
+    authController.login
+);
+
+// =====================
+// CASHIER
+// =====================
+router.get(
+    "/cashier",
+    auth,
+    role("cashier"),
+    (req, res) => {
+        res.json({
+            message: "Cashier Page"
+        });
+    }
+);
+
+
+// =====================
+// KITCHEN
+// =====================
+router.get(
+    "/kitchen",
+    auth,
+    role("kitchen"),
+    (req, res) => {
+        res.json({
+            message: "Kitchen Page"
+        });
+    }
+);
 
 module.exports = router;
